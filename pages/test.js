6f49775d68c9b65d4c14f8ea1bf526d3400afb23
westernavenue <141945337+Willwesternavenue@users.noreply.github.com>
2024-06-17 19:23:00 +0900

    wordpress.com

--- pages/test.js
+++ pages/test.js
@@ -1,4 +1,9 @@
-fetch('https://westernavenue.sakura.ne.jp/tenfour/wp-json/wp/v2/posts')
-  .then(response => response.json())
+fetch('https://public-api.wordpress.com/wp/v2/sites/incorporateservice.wordpress.com/posts/34')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
   .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+  .catch(error => console.error('Fetch error:', error));
